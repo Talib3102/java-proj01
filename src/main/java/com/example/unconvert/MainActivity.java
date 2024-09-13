cba326860d92cf0049477a06b5e27fca2802685d
Talib3102 <talibmohammadnaqiansari@gmail.com>
2024-09-13 11:34:11 +0530

    added

---
+++ src/main/java/com/example/unconvert/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.unconvert;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    private EditText editTextText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editTextText = findViewById(R.id.editTextText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Hi Click listner Worked", Toast.LENGTH_SHORT).show();
+                String s =editTextText.getText().toString();
+                int kg = Integer.parseInt(s);
+                double pound = 2.205 * kg;
+                textView.setText("The corresponding value in pounds is" + pound);
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
